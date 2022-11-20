a687ac58ac2697a081c4f04c3e3f14976dc160e9
dkzhen <dani314123@gmail.com>
2022-11-20 14:09:44 +0700

    First commit

---
+++ components/Projects.js
@@ -0,0 +1,76 @@
+import React from "react";
+import userData from "@constants/data";
+import Link from "next/link";
+
+export default function Projects() {
+  return (
+    <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          Projects
+        </h1>
+      </div>
+      {/* Grid starts here */}
+      {/* <div className="bg-[#F1F1F1] dark:bg-gray-900">
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40"> */}
+          {/* {userData.projects.map((proj, idx) => (
+            <ProjectCard
+              title={proj.title}
+              link={proj.link}
+              imgUrl={proj.imgUrl}
+              number={`${idx + 1}`}
+            />
+          ))} */}
+ <div className="bg-[#F1F1F1] dark:bg-gray-900">
+        <div className="mx-28  max-w-sm md:mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 pt-5  md:py-20 md:pb-40 ">
+            <Link
+            // href="/projects"
+            href="/"
+            className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-gray-700"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-arrow-up-right-square"
+              stroke="4"
+              strokeWidth="4"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"
+              />
+            </svg>
+            <p className="font-mono">Soon</p>
+          </Link>
+          </div>
+          </div>
+        {/* </div> */}
+      {/* </div> */}
+    </section>
+  );
+}
+
+const ProjectCard = ({ title, link, imgUrl, number }) => {
+  return (
+    <a href={link} className="w-full block shadow-2xl">
+      <div className="relative overflow-hidden">
+        <div className="h-72 object-cover">
+          <img
+            src={imgUrl}
+            alt="portfolio"
+            className="transform hover:scale-125 transition duration-2000 ease-out object-cover h-full w-full"
+          />
+        </div>
+        <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+          {title}
+        </h1>
+        <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+          {number.length === 1 ? "0" + number : number}
+        </h1>
+      </div>
+    </a>
+  );
+};
