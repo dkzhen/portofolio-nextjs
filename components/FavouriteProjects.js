379dd596597f9e795afe68c2e31ad36c44f299f6
dkzhen <dani314123@gmail.com>
2023-03-01 19:28:14 +0700

    update index project

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function FavouriteProjects() {
   return (
-    <div className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-10">
+    <div className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-10 ">
       <div className="max-w-6xl mx-auto">
-        <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
+        <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0 md:pb-12">
           <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-grey-700 dark:text-gray-600 text-center">
-          Projects
+            Projects
           </h1>
           <Link
             // href="/projects"
@@ -30,72 +30,72 @@ export default function FavouriteProjects() {
               />
             </svg>
             {/* <p>View all</p> */}
-            <p className="font-mono">Soon</p>
+            <p className="font-mono">View All Projects</p>
           </Link>
         </header>
 
         {/* Grid starts here */}
-        {/* <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40"> */}
+        <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
           {/* Single card */}
-          {/* <a
-            href="https://tailwindmasterkit.com"
+          <a
+            href="https://zhenmail.my.id"
             className="w-full block col-span-3 shadow-2xl"
           >
             <div className="relative overflow-hidden">
               <img
-                src="/tmk.jpg"
-                alt="portfolio"
+                src="/dkzhenmail.jpg"
+                alt="dkzhen - mail"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Tailwind Master Kit
+                DKZhen - Mail
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 01
               </h1>
             </div>
-          </a> */}
+          </a>
           {/* Single card */}
-          {/* <a
-            href="https://placeholdertech.in"
+          <a
+            href="https://github.com/dkzhen/noteist/releases/tag/V1.0"
             className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
           >
-            <div className="relative overflow-hidden"> */}
-              {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
-              {/* <img
-                src="/placeholdertech.png"
+            <div className="relative overflow-hidden">
+              <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div>
+              <img
+                src="/noteist.jpg"
                 alt="portfolio"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                PlaceholderTech
+                Noteist App
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 02
               </h1>
             </div>
-          </a> */}
+          </a>
           {/* Single card */}
-          {/* <a
-            href="https://manuarora.in"
+          <a
+            href="https:zhensoft.my.id"
             className="w-full block col-span-3 sm:col-span-1  object-cover"
           >
-            <div className="relative overflow-hidden shadow-2xl"> */}
-              {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
-              {/* <img
-                src="/portfolio.png"
-                alt="portfolio"
+            <div className="relative overflow-hidden shadow-2xl">
+              <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div>
+              <img
+                src="/store.jpg"
+                alt="store"
                 className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Portfolio
+                Store Products - API Dummy
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 03
               </h1>
             </div>
           </a>
-        </div> */}
+        </div>
       </div>
     </div>
   );
